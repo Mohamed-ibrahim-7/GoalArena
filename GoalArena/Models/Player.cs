d5aa70b5cd4254df5c9d5da08efa6dfe8928168f
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-04 21:08:51 +0300

    Creata Player

--- GoalArena/Models/Player.cs
+++ GoalArena/Models/Player.cs
@@ -2,5 +2,18 @@
 {
     public class Player
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Age { get; set; }
+        public string NationalTeam { get; set; }
+        public string Club { get; set; }
+        public int Goals { get; set; }
+        public int Assists { get; set; }            
+        public int MatchesPlayed { get; set; }
+        public double AverageRating { get; set; }
+        public string ImageUrl { get; set; }
+
+
     }
 }
