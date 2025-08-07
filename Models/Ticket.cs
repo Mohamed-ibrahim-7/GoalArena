730e8fa2e780ea85abf62a83ed284d4b2d9814ea
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-07 16:26:10 +0300

    Create Ticket.cs

---
+++ Models/Ticket.cs
@@ -0,0 +1,27 @@
+﻿namespace GoalArena.Models
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+
+        public int MatchId { get; set; }
+        public Match Match { get; set; }      = null!;
+
+        public string UserId { get; set; }    = null!;
+        public string SeatNumber { get; set; }     = null!;
+        public string TicketStatus { get; set; } = null!; 
+
+        public decimal Price { get; set; }
+        public DateTime PurchaseDate { get; set; }
+
+    } 
+    public enum TicketStatus
+    {
+    Available,
+    Pending,
+    reserved,
+    Sold,
+    Cancelled
+     }
+}
+     
