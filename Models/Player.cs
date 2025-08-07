e516945d8ee601b57885d6b1909d455b8d72fc07
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-07 16:25:52 +0300

    Create Player.cs

---
+++ Models/Player.cs
@@ -0,0 +1,19 @@
+﻿namespace GoalArena.Models
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }     = string.Empty;
+        public string Position { get; set; }   = string.Empty;
+        public int Age { get; set; }
+        public string NationalTeam { get; set; }   = string.Empty;
+        public int TeamId { get; set; }
+        public Team? Team { get; set; }
+        public int Goals { get; set; }
+        public int Assists { get; set; }            
+        public int MatchesPlayed { get; set; }
+        public string ImageUrl { get; set; }     = string.Empty;
+
+
+    }
+}
