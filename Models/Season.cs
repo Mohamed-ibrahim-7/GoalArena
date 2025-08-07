c9610fd16183c348508d5981ae0054e2cd7cd098
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-07 16:25:57 +0300

    Create Season.cs

---
+++ Models/Season.cs
@@ -0,0 +1,23 @@
+﻿namespace GoalArena.Models
+{
+    public class Season
+    {
+        public int SeasonId { get; set; }
+        public string Name { get; set; }     = null!;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int TournamentId { get; set; }
+        public Tournament Tournament { get; set; }       = null!;
+
+        public ICollection<Team> Teams { get; set; }     = new List<Team>();
+        public ICollection<Match> Matches { get; set; }   = new List<Match>();
+    }
+}
+public enum SeasonStatus
+{
+    Upcoming,
+    Ongoing,
+    Completed,
+    Cancelled
+}
