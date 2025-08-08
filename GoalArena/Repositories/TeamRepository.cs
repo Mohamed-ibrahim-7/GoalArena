e5d57000252e2312c4286a2f93f478295b7d606d
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-08 14:39:33 +0300

    Update TeamRepository.cs

--- GoalArena/Repositories/TeamRepository.cs
+++ GoalArena/Repositories/TeamRepository.cs
@@ -1,4 +1,5 @@
-﻿using GoalArena.DataAccess;
+﻿
+using GoalArena.Data;
 using GoalArena.Models;
 using GoalArena.Repositories.IRepositories;
 
@@ -6,8 +7,8 @@ namespace GoalArena.Repositories
 {
     public class TeamRepository : Repository<Team>, iTeamRepository
     {
-        private readonly ApplicationDbcontext _dbcontext;
-        public TeamRepository(ApplicationDbcontext context) : base(context)
+        private readonly ApplicationDbContext _dbcontext;
+        public TeamRepository(ApplicationDbContext context) : base(context)
         {
             _dbcontext = context;
         }
