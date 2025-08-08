d103c1ee56ff2ea9c3ac9914822eb20d4c791019
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-08 14:39:25 +0300

    Update TournamentRepository.cs

--- GoalArena/Repositories/TournamentRepository.cs
+++ GoalArena/Repositories/TournamentRepository.cs
@@ -1,4 +1,5 @@
-﻿using GoalArena.DataAccess;
+﻿using GoalArena.Data;
+
 using GoalArena.Models;
 using GoalArena.Repositories.IRepositories;
 
@@ -6,8 +7,8 @@ namespace GoalArena.Repositories
 {
     public class TournamentRepository : Repository<Tournament>, ITournamentRepository
     {
-        private readonly ApplicationDbcontext _dbcontext;
-        public TournamentRepository(ApplicationDbcontext context) : base(context)
+        private readonly ApplicationDbContext _dbcontext;
+        public TournamentRepository(ApplicationDbContext context) : base(context)
         {
             _dbcontext = context;
         }
