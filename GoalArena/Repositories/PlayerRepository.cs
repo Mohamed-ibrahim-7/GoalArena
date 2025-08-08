3b90cdb27caea6a7f9417cac77ea4d62722c4bde
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-08 14:39:43 +0300

    Update PlayerRepository.cs

--- GoalArena/Repositories/PlayerRepository.cs
+++ GoalArena/Repositories/PlayerRepository.cs
@@ -1,4 +1,5 @@
-﻿using GoalArena.DataAccess;
+﻿using GoalArena.Data;
+
 using GoalArena.Models;
 using GoalArena.Repositories.IRepositories;
 
@@ -6,8 +7,8 @@ namespace GoalArena.Repositories
 {
     public class PlayerRepository : Repository<Player>, IPlayerRepository
     {
-        private readonly ApplicationDbcontext _dbcontext;
-        public PlayerRepository(ApplicationDbcontext context) : base(context)
+        private readonly ApplicationDbContext _dbcontext;
+        public PlayerRepository(ApplicationDbContext context) : base(context)
         {
             _dbcontext = context;   
         }
