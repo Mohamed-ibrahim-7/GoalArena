d70935656cb3f22edbb9257ff3437d3e56179bbc
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-08 14:39:53 +0300

    Update MatchRepository.cs

--- GoalArena/Repositories/MatchRepository.cs
+++ GoalArena/Repositories/MatchRepository.cs
@@ -1,4 +1,5 @@
-﻿using GoalArena.DataAccess;
+﻿using GoalArena.Data;
+
 using GoalArena.Models;
 using GoalArena.Repositories.IRepositories;
 
@@ -6,9 +7,9 @@ namespace GoalArena.Repositories
 {
     public class MatchRepository : Repository<Match>, ImatchRepository 
     {
-        private readonly ApplicationDbcontext _context;
+        private readonly ApplicationDbContext _context;
 
-        public MatchRepository(ApplicationDbcontext context) : base(context)
+        public MatchRepository(ApplicationDbContext context) : base(context)
         {
             _context = context;
         }
