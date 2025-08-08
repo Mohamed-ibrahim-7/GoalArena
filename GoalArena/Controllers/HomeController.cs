882da8530ba6ec4a86138a7c560855f99b4b523a
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-08 14:39:06 +0300

    Create HomeController.cs

---
+++ GoalArena/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoalArena.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
