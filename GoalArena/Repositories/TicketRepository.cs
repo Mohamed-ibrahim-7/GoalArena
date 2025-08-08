745d628d020f6695485a8cfc57a57d0a9b4781d5
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-08 14:39:29 +0300

    Update TicketRepository.cs

--- GoalArena/Repositories/TicketRepository.cs
+++ GoalArena/Repositories/TicketRepository.cs
@@ -1,4 +1,5 @@
-﻿using GoalArena.DataAccess;
+﻿using GoalArena.Data;
+
 using GoalArena.Models;
 using GoalArena.Repositories.IRepositories;
 
@@ -6,8 +7,8 @@ namespace GoalArena.Repositories
 {
     public class TicketRepository : Repository<Ticket>, ITicketRepository
     {
-        private readonly ApplicationDbcontext _dbcontext;
-        public TicketRepository(ApplicationDbcontext context) : base(context)
+        private readonly ApplicationDbContext _dbcontext;
+        public TicketRepository(ApplicationDbContext context) : base(context)
         {
             _dbcontext = context;
         }
