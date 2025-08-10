9916c1895a5215411bac73b3e12ddd3805199d4b
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-11 02:05:36 +0300

    Add view Admin dashboard

--- GoalArena/Areas/Admin/Controllers/AdminController.cs
+++ GoalArena/Areas/Admin/Controllers/AdminController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GoalArena.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GoalArena.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            ViewBag.TotalSeasons = _context.Seasons.Count();
+            ViewBag.TotalTeams = _context.Teams.Count();
+            ViewBag.TotalTournaments = _context.Tournaments.Count();
+
             return View();
         }
     }
