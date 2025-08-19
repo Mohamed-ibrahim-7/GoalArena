71fece23e34ea901c7238012bc90ca8ad84b0f21
EL-Masria <EL-Masria@DESKTOP-60USKE8>
2025-08-19 03:53:14 +0300

    news controller

---
+++ GoalArena/ViewModels/NewWithRelatedNewsVM.cs
@@ -0,0 +1,10 @@
+﻿using GoalArena.Areas.Customer.Controllers;
+
+namespace GoalArena.ViewModels
+{
+    public class NewWithRelatedNewsVM
+    {
+        public News News { get; set; }
+        public List<News> RelatedNews { get; set; } = new List<News>();
+    }
+}
