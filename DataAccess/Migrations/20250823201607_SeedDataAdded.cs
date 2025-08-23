f7af5ba80efe5adf6fce8b7a237b6d1e3ae9f06a
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-23 23:19:09 +0300

    Add Data seed

---
+++ DataAccess/Migrations/20250823201607_SeedDataAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tournaments",
+                columns: new[] { "TournamentId", "Country", "LogoUrl", "Name" },
+                values: new object[] { 3, "أوروبا", "/images/tournaments/ucl.png", "دوري أبطال أوروبا" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tournaments",
+                keyColumn: "TournamentId",
+                keyValue: 3);
+        }
+    }
+}
---
+++ DataAccess/SeedData.cs
@@ -0,0 +1,23 @@
+﻿using GoalArena.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoalArena.Data.Seed
+{
+    public static class SeedData
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+           
+            modelBuilder.Entity<Tournament>().HasData(
+                new Tournament
+                {
+                    TournamentId = 3,
+                    Name = "دوري أبطال أوروبا",
+                    Country = "أوروبا",
+                    LogoUrl = "/images/tournaments/ucl.png"
+                }
+               
+            );
+        }
+    }
+}
